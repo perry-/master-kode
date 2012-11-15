eb4d5c4965e7e3176e1105e2c74af2662d85f5cb
Alexander Perry <yrrepxela@gmail.com>
2012-11-15 13:32:27 +0100

    added breadcrumbs

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 import xml.etree.ElementTree as ET
 import urllib2
 import json
-import codecs
 
 result = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.helsebiblioteket.no%2FRetningslinjer%2FHjerneslag%2FInnhold%22%20and%20xpath%3D%22%2F%2F*%5B%40id%3D'leftmenu'%5D%2F%2Fa%22&format=json").read()
 data = json.loads(result)
@@ -9,41 +8,44 @@ recommendation_ID = 0
 
 root = ET.Element("guidelines")
 
-
 def fetchMaintext(url):
-	maintext_data = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22"+urllib2.quote(url.encode("utf-8"))+"%22%20and%20xpath%3D%22%2F%2F*%5B%40id%3D'recommendations'%5D%2Fancestor%3A%3Adiv%2Fpreceding-sibling%3A%3Ah2%7C%2F%2F*%5B%40id%3D'recommendations'%5D%22&format=xml").read()
+	maintext_data = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22"+urllib2.quote(url.encode("utf-8"))+"%22%20and%20xpath%3D%22%2F%2F*%5B%40id%3D'recommendations'%5D%2Fancestor%3A%3Adiv%22").read()
 	try:
 		print("Trying to fetch main text")
 
 		results = ET.fromstring(maintext_data)
 		results_element = results.find("results")
 
-		if len(results_element) > 0:
+		if len(results.find("results")) > 0:
 			global recommendation_ID
 
-			ET.SubElement(root, "recommendations")
-			recommendations = root.find("recommendations")
-			recommendations.append(results_element)
+			print("Creating recommendation #"+str(recommendation_ID))
 
-			results_element.set("id", "recommendation"+str(recommendation_ID))
+			guidelinebreadcrumb = results_element.find(".//*[@id='guidelinebreadcrumb']")
+			heading = results_element.find("*/h2")
+			content = results_element.find(".//*[@id='recommendations']")
+
+			recommendation_element = ET.SubElement(root, "recommendation", {"id" : "recommendation"+str(recommendation_ID), "link" : url})
+			breadcrumb_element = ET.SubElement(recommendation_element, "guidelinebreadcrumb")
+			breadcrumb_element.extend(guidelinebreadcrumb)
+
+			recommendation_element.append(heading)
+			recommendation_element.append(content)
+
 			recommendation_ID += 1
 
 	except Exception, e:
 		print(e)
 
 def fetchURLs():
-	for x in xrange(1,10): #len(data['query']['results']['a'])):
+	no_urls = len(data['query']['results']['a'])
+	for x in xrange(1,10): #no_urls):
 		title = data['query']['results']['a'][x]['content']
 		url = data['query']['results']['a'][x]['href']
-		#title_node = dom.createTextNode(title)
-		#top_element.appendChild(title_node)
-		#top_element.appendChild(url.encode("utf-8"))
-		#text_file.write("<h3> %s </h3>"%title.encode("utf-8"))
-		#text_file.write("<a href="'%s'"> </a>\n"%url.encode("utf-8"))
-		print("Fetching recommendation "+str(x)+" from "+url)
+		print("Fetching recommendation from "+url+" ("+str(x)+"/"+str(no_urls)+")")
 		fetchMaintext(url)
 
-		tree = ET.ElementTree(root)
-		tree.write("results.xml")
+	tree = ET.ElementTree(root)
+	tree.write("results.xml")
 
 fetchURLs()
