2111f84540dd006539172dd409f7768f15c1fdea
Alexander Perry <yrrepxela@gmail.com>
2012-11-15 10:12:46 +0100

    changed from minidom to elementtree

--- parser.py
+++ parser.py
@@ -1,30 +1,31 @@
-from xml.dom.minidom import parseString, getDOMImplementation
+import xml.etree.ElementTree as ET
 import urllib2
 import json
 import codecs
 
 result = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.helsebiblioteket.no%2FRetningslinjer%2FHjerneslag%2FInnhold%22%20and%20xpath%3D%22%2F%2F*%5B%40id%3D'leftmenu'%5D%2F%2Fa%22&format=json").read()
 data = json.loads(result)
-dom = parseString("<?xml version='1.0' encoding='UTF-8' standalone='yes'?><guidelines></guidelines>")
-top_element = dom.documentElement
 recommendation_ID = 0
+
+root = ET.Element("guidelines")
+
 
 def fetchMaintext(url):
 	maintext_data = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22"+urllib2.quote(url.encode("utf-8"))+"%22%20and%20xpath%3D%22%2F%2F*%5B%40id%3D'recommendations'%5D%2Fancestor%3A%3Adiv%2Fpreceding-sibling%3A%3Ah2%7C%2F%2F*%5B%40id%3D'recommendations'%5D%22&format=xml").read()
 	try:
 		print("Trying to fetch main text")
-		dom = parseString(maintext_data)
-		maintext_results = dom.getElementsByTagName("results")[0]
 
-		if maintext_results.hasChildNodes():
+		results = ET.fromstring(maintext_data)
+		results_element = results.find("results")
+
+		if len(results_element) > 0:
 			global recommendation_ID
 
-			recommendations = dom.createElement("recommendations")
-			top_element.appendChild(recommendations)
-			recommendations.appendChild(maintext_results)
-			
-			maintext_results.setAttribute('id', 'recommendation'+str(recommendation_ID))
-			maintext_results.setIdAttribute('id')
+			ET.SubElement(root, "recommendations")
+			recommendations = root.find("recommendations")
+			recommendations.append(results_element)
+
+			results_element.set("id", "recommendation"+str(recommendation_ID))
 			recommendation_ID += 1
 
 	except Exception, e:
@@ -34,14 +35,15 @@ def fetchURLs():
 	for x in xrange(1,10): #len(data['query']['results']['a'])):
 		title = data['query']['results']['a'][x]['content']
 		url = data['query']['results']['a'][x]['href']
-		title_node = dom.createTextNode(title)
-		top_element.appendChild(title_node)
+		#title_node = dom.createTextNode(title)
+		#top_element.appendChild(title_node)
 		#top_element.appendChild(url.encode("utf-8"))
 		#text_file.write("<h3> %s </h3>"%title.encode("utf-8"))
 		#text_file.write("<a href="'%s'"> </a>\n"%url.encode("utf-8"))
 		print("Fetching recommendation "+str(x)+" from "+url)
 		fetchMaintext(url)
-	with codecs.open("results.xml", "w", "utf-8") as f:
-		top_element.writexml(f)
+
+		tree = ET.ElementTree(root)
+		tree.write("results.xml")
 
 fetchURLs()
