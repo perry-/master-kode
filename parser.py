7ae5294a40e1b0d434a46c8863a5ad7c25f60295
Alexander Perry <yrrepxela@gmail.com>
2012-11-15 15:11:23 +0100

    added comments

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import urllib2
 import json
 
+# Load urls from table of contents (http://www.helsebiblioteket.no/retningslinjer/hjerneslag/innhold)
 result = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.helsebiblioteket.no%2FRetningslinjer%2FHjerneslag%2FInnhold%22%20and%20xpath%3D%22%2F%2F*%5B%40id%3D'leftmenu'%5D%2F%2Fa%22&format=json").read()
 data = json.loads(result)
 recommendation_ID = 0
@@ -9,6 +10,7 @@ recommendation_ID = 0
 root = ET.Element("guidelines")
 
 def fetchMaintext(url):
+	# Parse url with xpath="//*[@id='recommendations']/ancestor::div" (Finds all chapters with a recommendation)
 	maintext_data = urllib2.urlopen("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22"+urllib2.quote(url.encode("utf-8"))+"%22%20and%20xpath%3D%22%2F%2F*%5B%40id%3D'recommendations'%5D%2Fancestor%3A%3Adiv%22").read()
 	try:
 		print("Trying to fetch main text")
@@ -21,12 +23,12 @@ def fetchMaintext(url):
 
 			print("Creating recommendation #"+str(recommendation_ID))
 
-			guidelinebreadcrumb = results_element.find(".//*[@id='guidelinebreadcrumb']")
-			heading = results_element.find("*/h2")
-			content = results_element.find(".//*[@id='recommendations']")
+			guidelinebreadcrumb = results_element.find(".//*[@id='guidelinebreadcrumb']") # Find first element with id="guidelinebreadcrumb"
+			heading = results_element.find("*/h2") # Find first element with tag "h2"
+			content = results_element.find(".//*[@id='recommendations']") # Find first element with id="recommendations"
 
-			recommendation_element = ET.SubElement(root, "recommendation", {"id" : "recommendation"+str(recommendation_ID), "link" : url})
-			breadcrumb_element = ET.SubElement(recommendation_element, "guidelinebreadcrumb")
+			recommendation_element = ET.SubElement(root, "recommendation", {"id" : "recommendation"+str(recommendation_ID), "link" : url}) # Create recommendation element with id and link as attributes
+			breadcrumb_element = ET.SubElement(recommendation_element, "guidelinebreadcrumb") 
 			breadcrumb_element.extend(guidelinebreadcrumb)
 
 			recommendation_element.append(heading)
@@ -39,13 +41,15 @@ def fetchMaintext(url):
 
 def fetchURLs():
 	no_urls = len(data['query']['results']['a'])
-	for x in xrange(1,10): #no_urls):
+	for x in xrange(1,no_urls):
 		title = data['query']['results']['a'][x]['content']
 		url = data['query']['results']['a'][x]['href']
 		print("Fetching recommendation from "+url+" ("+str(x)+"/"+str(no_urls)+")")
 		fetchMaintext(url)
 
+	# Create element tree from root element its children
 	tree = ET.ElementTree(root)
+	# Write tree to file
 	tree.write("results.xml")
 
 fetchURLs()
